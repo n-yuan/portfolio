fe34ee1900c025af0341298f96bb1a4742c92ee6
Nan Yuan <nan.yuan.dev@gmail.com>
2020-10-03 20:03:17 +1000

    Update projects section

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -4,21 +4,16 @@ import TeamLogger from "../images/team-logger.png";
 const Projects = () => {
   return (
     <div id="projects" className="projects-container">
-      <div className="projects-heading-wrapper">
-        <p className="projects-caption">View my projects</p>
-        <h1 className="projects-header"> Projects</h1>
-        <div className="projects-dash mx-auto"></div>
-      </div>
-      <div className="projects-wrapper">
-        <div className="single-project-wrapper">
-          <h1>Team Logger</h1>
-          <div className="row">
-            <div className="col-lg-6">
-              <img
-                src={TeamLogger}
-                alt="team-logger"
-                style={{ width: "600px" }}
-              />
+      <div className="single-project-wrapper">
+        <h1 className="single-project-header">Team Logger</h1>
+        <div className="row">
+          <div className="col-lg-6">
+            <img
+              src={TeamLogger}
+              alt="team-logger"
+              className="single-project-image"
+            />
+            <div className="row">
               <div className="col-lg-6">
                 <button className="view-project-btn">VIEW PROJECT</button>
               </div>
@@ -26,17 +21,20 @@ const Projects = () => {
                 <button className="view-code-btn"> VIEW CODE</button>
               </div>
             </div>
-            <div className="col-lg-6">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-                similique dolor nam, voluptate ipsa blanditiis deserunt placeat
-                nulla sequi at dolores mollitia? Deserunt asperiores at totam
-                saepe esse ex debitis?Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Amet similique dolor nam, voluptate ipsa
-                blanditiis deserunt placeat nulla sequi at dolores mollitia?
-                Deserunt asperiores at totam saepe esse ex debitis?
-              </p>
-            </div>
+          </div>
+          <div className="col-lg-6">
+            <h2 className="single-project-description-header">
+              My portfolio website.
+            </h2>
+            <p className="single-project-description-content">
+              • Used CSS3, Sass, HTML5, JavaScript, React.js, React hooks,
+              Bootstrap, Responsive design for front-end development
+              development.
+            </p>
+            <p className="single-project-description-content">
+              •Used Node.js, MongoDB, Express, Mongoose, RESTful API for
+              back-end development.
+            </p>
           </div>
         </div>
       </div>
---
+++ src/component/ProjectsHeading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProjectsHeading = () => {
+  return (
+    <div className="projects-heading-wrapper">
+      <p className="projects-caption">View my projects</p>
+      <h1 className="projects-header"> Projects</h1>
+      <div className="projects-dash mx-auto"></div>
+    </div>
+  );
+};
+
+export default ProjectsHeading;
