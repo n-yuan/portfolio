7b0b3afc5142e5c8872fc006c49b3fed743d2e98
Nan Yuan <nan.yuan.dev@gmail.com>
2020-10-04 22:22:24 +1100

    Add Name bg effect

--- src/component/Home.js
+++ src/component/Home.js
@@ -16,10 +16,10 @@ const Home = () => {
           minWidth: 200.0,
           highlightColor: 0x41a9f2,
           midtoneColor: 0x61cf,
-          lowlightColor: 0xffffff,
+          lowlightColor: 0xb1b1b1,
           baseColor: 0xffffff,
-          blurFactor: 0.24,
-          speed: 4.4,
+          blurFactor: 0.35,
+          speed: 5.0,
           zoom: 3.0,
         })
       );
@@ -42,16 +42,18 @@ const Home = () => {
       ></div>
       <div className="home-wrapper">
         <div className="background-img"></div>
-        <h4 className="home-greeting">Hello, I'm</h4>
-        <h1 className="home-name">NAN YUAN</h1>
-        <h4 className="home-title">
-          Full-stack developer| Front-end developer
-        </h4>
-        <div className="icons-wrapper">
-          <i class="fab fa-linkedin fa-lg icon-linkedin"></i>
-          <i class="fab fa-github fa-lg icon-github"></i>
-          <i class="fas fa-at icon-at fa-lg "></i>
-          <i class="fas fa-id-card icon-id-card fa-lg "></i>
+        <div>
+          <h4 className="home-greeting">Hello, I'm</h4>
+          <h1 className="home-name">NAN YUAN</h1>
+          <h4 className="home-title">
+            Full-stack developer| Front-end developer
+          </h4>
+          <div className="icons-wrapper">
+            <i class="fab fa-linkedin fa-lg icon-linkedin"></i>
+            <i class="fab fa-github fa-lg icon-github"></i>
+            <i class="fas fa-at icon-at fa-lg "></i>
+            <i class="fas fa-id-card icon-id-card fa-lg "></i>
+          </div>
         </div>
       </div>
     </div>
