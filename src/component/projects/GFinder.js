4aaa904cd79a2843e8658bef30bd9c4de7c7bd0c
Nan Yuan <nan.yuan.dev@gmail.com>
2020-10-18 03:08:59 +1100

    Update g-finder info

--- src/component/projects/GFinder.js
+++ src/component/projects/GFinder.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
 import TeamLoggerImg_1 from "../../images/team-logger.png";
-import TeamLoggerImg_2 from "../../images/members.png";
-import TeamLoggerImg_3 from "../../images/add-members.png";
 
 import AwesomeSlider from "react-awesome-slider";
 import "react-awesome-slider/dist/styles.css";
@@ -19,8 +17,8 @@ const TeamLogger = () => {
             bullets={false}
           >
             <div data-src={TeamLoggerImg_1} />
-            <div data-src={TeamLoggerImg_2} />
-            <div data-src={TeamLoggerImg_3} />
+            <div data-src={TeamLoggerImg_1} />
+            <div data-src={TeamLoggerImg_1} />
           </AwesomeSlider>
           <div className="row">
             <div className="col-lg-6">
@@ -33,10 +31,13 @@ const TeamLogger = () => {
         </div>
         <div className="col-lg-6">
           <h2 className="single-project-description-header">
-            A web application that could search Github users, get their
-            information and repositories, explore the trending projects on
-            GitHub, and sort trending projects by languages and time
+            A web application help you find top things for Github.
           </h2>
+          <p className="single-project-description-content">
+            • A web application that could find GitHub Trending Repositories,
+            search Github users, get their information and repositories, explore
+            the trending projects on GitHub
+          </p>
           <p className="single-project-description-content">
             • Used CSS3, Sass, HTML5, JavaScript, React.js, React hooks,
             Bootstrap, Responsive design for front-end development
