ea9211524c626c7de496e5e7f2de4c1cc067ef16
Nan Yuan <nan.yuan.dev@gmail.com>
2020-10-18 05:29:25 +1100

    Scroll to top hide at home, update icon

--- src/component/ScrollToTop.js
+++ src/component/ScrollToTop.js
@@ -1,13 +1,34 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 
 const ScrollToTop = () => {
-  useEffect(() => {});
+  const [showBtn, setShowBtn] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", bindHandleScroll);
+  });
+
+  const bindHandleScroll = (e) => {
+    const scrollTop =
+      (e.srcElement ? e.srcElement.documentElement.scrollTop : false) ||
+      window.pageYOffset ||
+      (e.srcElement ? e.srcElement.body.scrollTop : 0);
+
+    if (scrollTop > 0 && scrollTop <= 800) {
+      setShowBtn(false);
+    } else if (scrollTop > 800 && scrollTop <= 1600) {
+      setShowBtn(true);
+    }
+  };
+
   return (
     <div style={{ position: "fixed", right: "10px", bottom: "10px" }}>
-      <Link to="home" spy={true} smooth={true} duration={500}>
-        <button>Top</button>
-      </Link>
+      {showBtn && (
+        <Link to="home" spy={true} smooth={true} duration={500}>
+          {/* <button>Top</button> */}
+          <i class="fas fa-angle-up scroll-to-top-btn"></i>
+          {/* <i class="fab fa-linkedin fa-lg icon-linkedin"></i> */}
+        </Link>
+      )}
     </div>
   );
 };
